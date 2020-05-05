f0842d373bd0f69d9ff02b851e93fec1c9dcb297
JordonBruton <jrb123@humbolt.edu>
2020-05-04 20:38:45 -0700

    update

--- CreatingAComputer/CreatingAComputer/Processors.cpp
+++ CreatingAComputer/CreatingAComputer/Processors.cpp
@@ -9,7 +9,10 @@ void Processors::PickingOutProcessor()
 	unordered_map<double, string> amd_processor{};
 	unordered_map<double, string> intel_processor{};
 	LocalVariables local;
-	
+	Processors::PickingOutProcessor(double& local.budget)
+	{
+
+	}
 
 	amd_processor = {
 		{432.41, "AMD Ryzen 9 3900x"},
@@ -31,7 +34,7 @@ void Processors::PickingOutProcessor()
 
 	cout << "Pick out a processor brand: Intel, AMD" << endl;
 	cin >> local.processor_brand;
-	cout << local.getBudget() << endl;
+
 	
 	if (local.processor_brand == "AMD" || local.processor_brand == "amd")
 	{
